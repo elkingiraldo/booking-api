425baaf279656d29cd9b9715290499502d7d1f1c
egiraldo <elkin.giraldo.pinedo@gmail.com>
2022-03-02 21:46:52 -0500

    :heavy_plus_sign: add java docs on swagger

--- src/main/java/co/com/elkin/apps/bookingapi/configuration/SwaggerConfig.java
+++ src/main/java/co/com/elkin/apps/bookingapi/configuration/SwaggerConfig.java
@@ -1,16 +1,12 @@
 package co.com.elkin.apps.bookingapi.configuration;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Configuration for swagger documentation
@@ -18,32 +14,32 @@ import java.util.Set;
  * @author egiraldo
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     private static final String MAINTAINER_NAME = "Elkin Giovanni Giraldo Pinedo";
     private static final String MAINTAINER_EMAIL = "elkin.giraldo.pinedo@gmail.com";
     private static final String MAINTAINER_GITHUB = "https://github.com/elkingiraldo";
 
-    private static final Set<String> DEFAULT_PRODUCES = new HashSet<>(Collections.singletonList("application/json"));
-    private static final Set<String> DEFAULT_CONSUMES = new HashSet<>(Collections.singletonList("application/json"));
-
     private static final String TITLE = "Api Documentation for Booking API";
     private static final String DESCRIPTION = "This module is in charge of managing all request to Booking API";
-    private static final String VERSION = "1.0";
-    private static final String TOS_URI = "urn:tos";
+    private static final String VERSION = "v0.0.1";
     private static final String LICENSE = "All rights reserved";
+    private static final String LICENSE_URI = "https://springdoc.org";
 
-    public static final Contact DEFAULT_CONTACT = new Contact(MAINTAINER_NAME, MAINTAINER_GITHUB, MAINTAINER_EMAIL);
-    public static final ApiInfo DEFAULT = new ApiInfo(TITLE, DESCRIPTION, VERSION, TOS_URI, DEFAULT_CONTACT, LICENSE,
-            MAINTAINER_EMAIL, Collections.emptyList());
+    private static final String GITHUB_README = "GitHub Booking API Documentation";
+    private static final String GITHUB_README_URI = "https://github.com/elkingiraldo/booking-api/blob/main/README.md";
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT)
-                .produces(DEFAULT_PRODUCES)
-                .consumes(DEFAULT_CONSUMES);
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION)
+                        .license(new License().name(LICENSE).url(LICENSE_URI))
+                        .contact(new Contact().name(MAINTAINER_NAME).email(MAINTAINER_EMAIL).url(MAINTAINER_GITHUB)))
+                .externalDocs(new ExternalDocumentation()
+                        .description(GITHUB_README)
+                        .url(GITHUB_README_URI));
     }
 
 }
